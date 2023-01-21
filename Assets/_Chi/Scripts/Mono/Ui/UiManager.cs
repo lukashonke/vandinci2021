c6ef792668739c28f905202f6a47e3aa3a5f1fef
Lukas Honke <honke.lukas@gmail.com>
2023-01-21 16:26:53 +0100

    ui
    ui

--- Assets/_Chi/Scripts/Mono/Ui/UiManager.cs
+++ Assets/_Chi/Scripts/Mono/Ui/UiManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Chi.Scripts.Mono.Ui;
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
     public TextMeshProUGUI benchmarkStats;
+
+    [Required] public VehicleSettingsWindow vehicleSettingsWindow; 
     
     // Start is called before the first frame update
     void Start()
---
+++ Assets/_Chi/Scripts/Mono/Ui/VehicleSettingsWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Chi.Scripts.Mono.Ui
+{
+    public class VehicleSettingsWindow : MonoBehaviour
+    {
+        [Required] public GameObject moduleSlots;
+        [Required] public GameObject moduleSlot;
+
+        public void Awake()
+        {
+            
+        }
+    }
+}
